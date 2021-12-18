2f4a1da4690777245fb83ab19f199a77dac72ad4
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2021-12-18 15:43:03 -0800

    Create closures.js

---
+++ closures.js
@@ -0,0 +1,29 @@
+function a() {
+    let grandpa = 'grandpa'
+    return function b() {
+        let father = 'father'
+        return function c() {
+            let son = 'son'
+            return `${grandpa} > ${father} > ${son}`
+        }
+    }
+}
+
+const first = a()()()
+consol.log(first)
+
+
+const boo = string => name => name2 => console.log(`${string} ${name} ${name2}`)
+boo('hey')('tim')('david')
+// output:  hey tim david
+
+
+
+function callMeMaybe() {
+    const callMe = 'Hey there I am now here!'
+    setTimeout(function () {  // this is the closure
+        console.log(callMe)
+    }, 4000)
+}
+
+callMeMaybe();
