5315dca397a56974e23e6c0aa24dc771a26c3b27
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2022-01-12 17:37:57 -0800

    Update closures.js

--- closures.js
+++ closures.js
@@ -17,8 +17,6 @@ const boo = string => name => name2 => console.log(`${string} ${name} ${name2}`)
 boo('hey')('tim')('david')
 // output:  hey tim david
 
-
-
 function callMeMaybe() {
     const callMe = 'Hey there I am now here!'
     setTimeout(function () {  // this is the closure
