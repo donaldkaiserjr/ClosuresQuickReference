4e5ea78f27ca86f5b37626bcb3c6eecf12cbfcc9
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2022-01-12 17:38:50 -0800

    Update closures.js

--- closures.js
+++ closures.js
@@ -12,7 +12,6 @@ function a() {
 const first = a()()()
 consol.log(first)
 
-
 const boo = string => name => name2 => console.log(`${string} ${name} ${name2}`)
 boo('hey')('tim')('david')
 // output:  hey tim david
